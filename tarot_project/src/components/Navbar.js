fc9e168e5cdf2f041b1c2907fb30dc21bf30190a
Alexis Tamayo <lexx426@gmail.com>
2024-04-22 12:16:51 -0500

    Set up
    Removed unnecessary files, added imports for tailwind and scss

---
+++ tarot_project/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <h2 className=''>Tarot Temple</h2>
+        <p>History</p>
+        <p>Cards</p>
+        {/* create drop down menu, All, major, minor, etc */}
+        <p>Quiz</p>
+        <button>Log In</button>
+
+    </div>
+  )
+}
+
+export default Navbar
