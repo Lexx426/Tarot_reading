b747d4d8a934ea83d5b5f3f970df81b023752293
Alexis Tamayo <lexx426@gmail.com>
2024-04-24 17:09:57 -0500

    Tailwind
    Added the last needed for tailwind and started to design navbar

--- tarot_project/src/components/Navbar.js
+++ tarot_project/src/components/Navbar.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-        <h2 className=''>Tarot Temple</h2>
-        <p>History</p>
-        <p>Cards</p>
-        {/* create drop down menu, All, major, minor, etc */}
-        <p>Quiz</p>
-        <button>Log In</button>
+    <div >
+        <nav >
+            <h2 className='text-3xl  '>Tarot Temple</h2>
+            <p>History</p>
+            <p>Cards</p>
+            {/* create drop down menu, All, major, minor, etc */}
+            <p>Quiz</p>
+            <button>Log In</button>
+        </nav>
+
 
     </div>
   )
