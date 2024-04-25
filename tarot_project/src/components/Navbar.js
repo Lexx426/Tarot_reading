7e7a149f30816c0c7acad1deb67490633bcf4363
Alexis Tamayo <lexx426@gmail.com>
2024-04-25 13:49:19 -0500

    Button
    created the design for the login button

--- tarot_project/src/components/Navbar.js
+++ tarot_project/src/components/Navbar.js
@@ -3,13 +3,13 @@ import React from 'react'
 function Navbar() {
   return (
     <div >
-        <nav >
+        <nav className='flex gap-5'>
             <h2 className='text-3xl  '>Tarot Temple</h2>
-            <p>History</p>
+            <a>History</a>
             <p>Cards</p>
             {/* create drop down menu, All, major, minor, etc */}
             <p>Quiz</p>
-            <button>Log In</button>
+            <button className='bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Log In</button>
         </nav>
 
 
