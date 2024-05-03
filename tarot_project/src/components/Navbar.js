5ced768691b28ac9c99a5e440e32884029a62897
Alexis Tamayo <lexx426@gmail.com>
2024-05-03 14:53:26 -0500

    Navbar
    Added some basic structoring and spacing for navbar

--- tarot_project/src/components/Navbar.js
+++ tarot_project/src/components/Navbar.js
@@ -3,13 +3,16 @@ import React from 'react'
 function Navbar() {
   return (
     <div >
-        <nav className='flex gap-5'>
-            <h2 className='text-3xl  '>Tarot Temple</h2>
-            <a>History</a>
-            <p>Cards</p>
-            {/* create drop down menu, All, major, minor, etc */}
-            <p>Quiz</p>
-            <button className='bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Log In</button>
+        <nav className='flex  w-screen items-center justify-around z-10 fixed h-14 border border-solid border-red-700'>
+            <h2 className='text-3xl '>Tarot Temple</h2>
+            <div className='flex items-center w-2/5 justify-evenly border border-solid border-green-700'>
+                <a>History</a>
+                <a>Cards</a>
+                {/* create drop down menu, All, major, minor, etc */}
+                <a>Quiz</a>
+                <button className='bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Log In</button>
+            </div>
+            
         </nav>
 
 
