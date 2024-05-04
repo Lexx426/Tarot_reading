e4d5f401a9067f8be1462defaaf9d3dae541e387
Alexis Tamayo <lexx426@gmail.com>
2024-05-04 10:44:34 -0500

    Mobile Navbar
     Created the X that will act as a button to close the mobile navbar with tailwind

--- tarot_project/src/components/MobileNav.js
+++ tarot_project/src/components/MobileNav.js
@@ -6,9 +6,23 @@ function MobileNav() {
         <nav>
             <section>
                 <div className='space-y-3'>
-                    <span className='block w-10 h-5 bg-red-400'></span>
-                    <span className='block w-10 h-5 bg-red-400'></span>
-                    <span className='block w-10 h-5 bg-red-400'></span>
+                    <span className='block w-10 h-0.5 bg-red-400'></span>
+                    <span className='block w-10 h-0.5 bg-red-400'></span>
+                    <span className='block w-10 h-0.5 bg-red-400'></span>
+                </div>
+                <div className='border '>
+                    <svg
+                        className='h-12 w-12 text-gray-600'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        stroke='currentColor'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        b
+                    >
+                        <line x1={18} y1={6} x2={6} y2={18}/>
+                        <line x1={6} y1={6} x2={18} y2={18}/>
+                    </svg>
                 </div>
             </section>
             
