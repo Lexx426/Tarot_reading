7005db45c989660c4215d267cf0701886c864687
Alexis Tamayo <lexx426@gmail.com>
2024-05-09 16:47:34 -0500

    Navbar
    Completed the functionality of the mobile navbar hamburger menu and exit of the mobile navbar

--- tarot_project/src/components/MobileNav.js
+++ tarot_project/src/components/MobileNav.js
@@ -1,18 +1,21 @@
 import React, {useState} from 'react'
+import '../scss/navbar.scss'
 
 function MobileNav() {
     const [MobileNav, setMobileNav] = useState(false)
   return (
-    <div className="flex items-center justify-between border-b border-gray-400 py-8">
+    <div className="flex items-center justify-center border-b border-gray-400 py-8">
+        <h2 className='text-3xl md:hidden'>Tarot Temple</h2>
         <nav >
             <section className='flex md:hidden'>
-                <div className='space-y-2' onClick={() => setMobileNav((prev) => !prev)}>
+            
+                <div className='space-y-3 absolute top-0 right-0 px-8 py-8 cursor-pointer' onClick={() => setMobileNav((prev) => !prev)}>
                     <span className='block w-10 h-0.5 animate-bounce bg-red-400'></span>
                     <span className='block w-10 h-0.5 animate-bounce bg-red-400'></span>
                     <span className='block w-10 h-0.5 animate-bounce bg-red-400'></span>
                 </div>
-                <div className='border '>
-                    <div className="absolute top-0 right-0 px-8 py-8">
+                <div className={MobileNav ? "showMenuNav" : "hideMenuNav"} >
+                    <div className="absolute top-0 right-0 px-8 py-8 cursor-pointer" onClick={() => setMobileNav(false)}>
                         <svg
                             className='h-12 w-12 text-gray-600'
                             viewBox='0 0 24 24'
@@ -26,8 +29,6 @@ function MobileNav() {
                             <line x1={6} y1={6} x2={18} y2={18}/>
                         </svg>
                     </div>
-                    
-                
                     <ul className="NAVIGATION-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
                         <li className="border-b border-gray-400 my-8 uppercase">
                             <a href='/'>
