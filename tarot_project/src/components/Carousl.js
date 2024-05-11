9cded5695e40b61ffbca188e8a8afe0f60261a45
Alexis Tamayo <lexx426@gmail.com>
2024-05-11 13:44:59 -0500

    Carousel
    Centered images. Added styled accessibility previous and next arrows.

--- tarot_project/src/components/Carousl.js
+++ tarot_project/src/components/Carousl.js
@@ -6,7 +6,7 @@ import '../scss/Carousel.scss'
 
 function Carousl() {
     const settings = {
-        accessibility: true,
+        // accessibility: true,
         // autoplay: true,
         // centerMode: true,
         dots: true,
@@ -14,21 +14,38 @@ function Carousl() {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
+
+        nextArrow: (
+            <div >
+              <div className="next-slick-arrows">
+                  <svg xmlns="http://www.w3.org/2000/svg" stroke="black" height="30" viewBox="0 -960 960 960" width="30"><path d="m242-200 200-280-200-280h98l200 280-200 280h-98Zm238 0 200-280-200-280h98l200 280-200 280h-98Z"/></svg>
+              </div>
+            </div>
+          ),
+      
+          prevArrow: (
+            <div>
+              <div className="next-slick-arrow rotate-180">
+                <svg xmlns="http://www.w3.org/2000/svg" stroke="black" height="30" viewBox="0 -960 960 960" width="30"><path d="m242-200 200-280-200-280h98l200 280-200 280h-98Zm238 0 200-280-200-280h98l200 280-200 280h-98Z"/></svg>
+              </div>
+            </div>
+          ),
     }
   return (
-    <div className='lg (1024px) border border-black m-4 p-4 '>
+    <div className=' border border-black m-8 px-18'>
         <Slider {...settings}>
-            <div className=' '>
-                {/* <h3 className='bg-red-300 w-50 flex justify-center '>1</h3> */}
-                <img className='w-15  'src='../tarot-file/img/c01.jpg' alt='card1'/>
+            
+            <div className='flex justify-center items-center '>
+                {/* <h3>1</h3> */}
+                <img  src='../tarot-file/img/c01.jpg' alt='card1' style={{ margin: 'auto' }}/>
             </div>
-            <div>
-                <h3>2</h3>
-                <img src='../tarot-file/img/c01.jpg' alt='card1'/>
+            <div className='flex justify-center items-center '>
+            {/* <h3 className='bg-red-300 w-50 flex justify-center '>2</h3> */}
+                <img src='../tarot-file/img/c02.jpg' alt='card1' style={{ margin: 'auto' }}/>
             </div>
-            <div>
-                <h3>3</h3>
-                <img src='../tarot-file/img/c01.jpg' alt='card1'/>
+            <div className='flex justify-center items-center '>
+            {/* <h3>3</h3> */}
+                <img src='../tarot-file/img/c03.jpg' alt='card1' style={{ margin: 'auto' }}/>
             </div>
         </Slider>
     </div>
--- tarot_project/src/components/MainBody.js
+++ tarot_project/src/components/MainBody.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Carousl from './Carousl'
 
 function MainBody() {
   return (
     <div>
-        <h1> Welcome to the world of Tarot!</h1>
+        <h1 > Welcome to the world of Tarot!</h1>
         <p> We hope that this website can bring you knowledge about what exactly is Tarot</p>
+        <Carousl/>
     </div>
   )
 }
