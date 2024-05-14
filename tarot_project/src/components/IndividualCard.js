7c91f0cb6cc837fcb0b736620a645dd31c8dc745
Alexis Tamayo <lexx426@gmail.com>
2024-05-13 20:14:39 -0500

    Img folder
    moved image and json file into src folder to allow for easier ability to retrieve info

--- tarot_project/src/components/IndividualCard.js
+++ tarot_project/src/components/IndividualCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import tarotData from '/public/tarot-file/tarot-images.json'
+import tarotData from '../tarot-file/tarot-images.json'
 
 function IndividualCard() {
-    console.log(tarotData.name)
+  console.log(tarotData.name)
   return (
     <div>
-            {tarotData.name}
+      {tarotData.name}
     </div>
   )
 }
