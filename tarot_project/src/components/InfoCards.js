b51f5b7f4330b807e13b8668c633c207a22dac71
Alexis Tamayo <lexx426@gmail.com>
2024-06-10 12:55:17 -0500

    Infocard Props

--- tarot_project/src/components/InfoCards.js
+++ tarot_project/src/components/InfoCards.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function InfoCards(props) {
-    const {name, description} = props
+    const {name, description, picture} = props
   return (
     <div>
         <h3> {name} </h3>
+        <p> {description} </p>
     </div>
   )
 }
--- tarot_project/src/components/MainBody.js
+++ tarot_project/src/components/MainBody.js
@@ -15,9 +15,9 @@ function MainBody() {
           <p> 
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <InfoCards name="Alexis"/>
-          <InfoCards name="Rachel"/>
-          <InfoCards name="Emily"/>
+          <InfoCards name="Alexis" description="Creator of website"/>
+          <InfoCards name="Rachel" description="Psychic"/>
+          <InfoCards name="Emily" description="Psychic and Witch"/>
 
 
         </div>
