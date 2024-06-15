0f0a8e72aecfddaa29b84fa7e1ecbaf979115b3f
Alexis Tamayo <lexx426@gmail.com>
2024-06-15 10:19:33 -0500

    border

--- tarot_project/src/components/InfoCards.js
+++ tarot_project/src/components/InfoCards.js
@@ -3,7 +3,7 @@ import React from 'react'
 function InfoCards(props) {
     const {name, description, picture} = props
   return (
-    <div className='column-3 border '>
+    <div className='column-3 border  border-spacing-1'>
         <h3> {name} </h3>
         <p> {description} </p>
     </div>
